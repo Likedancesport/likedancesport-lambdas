7a3339d01faaa19e793e2f70c47130dc62546aa2
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-04-11 13:23:43 +0300

    let's try

--- lambda-base-layer/video-upload-handler/src/main/java/com/likedancesport/VideoUploadHandlerApp.java
+++ lambda-base-layer/video-upload-handler/src/main/java/com/likedancesport/VideoUploadHandlerApp.java
@@ -1,13 +1,12 @@
 package com.likedancesport;
 
-import com.likedancesport.common.model.impl.Video;
 import com.likedancesport.service.IVideoEncodingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class VideoUploadHandlerApp {
     public static void main(String[] args) {
-        IVideoEncodingService bean = SpringApplication.run(Main.class, args).getBean(IVideoEncodingService.class);
+        IVideoEncodingService bean = SpringApplication.run(VideoUploadHandlerApp.class, args).getBean(IVideoEncodingService.class);
     }
 }
