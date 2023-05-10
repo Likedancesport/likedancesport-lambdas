37e68e3f2b071990213ec51026b23475cd1b4397
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-05-10 12:24:34 +0300

    returned to jdk 11 and spring boot 2.7.8

--- compute-infra/src/main/java/com/likedancesport/ComputeServerlessStack.java
+++ compute-infra/src/main/java/com/likedancesport/ComputeServerlessStack.java
@@ -3,70 +3,14 @@ package com.likedancesport;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.App;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.apigateway.AuthorizationType;
-import software.amazon.awscdk.services.apigateway.Integration;
-import software.amazon.awscdk.services.apigateway.LambdaIntegration;
-import software.amazon.awscdk.services.apigateway.Method;
-import software.amazon.awscdk.services.apigateway.MethodOptions;
-import software.amazon.awscdk.services.apigateway.Resource;
-import software.amazon.awscdk.services.apigateway.RestApi;
-import software.amazon.awscdk.services.apigateway.StageOptions;
-import software.amazon.awscdk.services.cloudfront.BehaviorOptions;
-import software.amazon.awscdk.services.cloudfront.Distribution;
-import software.amazon.awscdk.services.cloudfront.PriceClass;
-import software.amazon.awscdk.services.cloudfront.origins.S3Origin;
-import software.amazon.awscdk.services.events.EventBus;
-import software.amazon.awscdk.services.events.EventPattern;
-import software.amazon.awscdk.services.events.IEventBus;
-import software.amazon.awscdk.services.events.IRuleTarget;
-import software.amazon.awscdk.services.events.Rule;
-import software.amazon.awscdk.services.events.targets.SnsTopic;
-import software.amazon.awscdk.services.iam.IRole;
-import software.amazon.awscdk.services.iam.Role;
-import software.amazon.awscdk.services.lambda.Alias;
-import software.amazon.awscdk.services.lambda.Architecture;
-import software.amazon.awscdk.services.lambda.CfnFunction;
-import software.amazon.awscdk.services.lambda.Code;
-import software.amazon.awscdk.services.lambda.Function;
-import software.amazon.awscdk.services.lambda.HttpMethod;
-import software.amazon.awscdk.services.lambda.IFunction;
-import software.amazon.awscdk.services.lambda.LayerVersion;
-import software.amazon.awscdk.services.lambda.Runtime;
-import software.amazon.awscdk.services.lambda.Version;
-import software.amazon.awscdk.services.lambda.eventsources.SqsEventSource;
-import software.amazon.awscdk.services.mediaconvert.CfnQueue;
-import software.amazon.awscdk.services.s3.BlockPublicAccess;
-import software.amazon.awscdk.services.s3.Bucket;
-import software.amazon.awscdk.services.s3.BucketAccessControl;
-import software.amazon.awscdk.services.s3.BucketEncryption;
-import software.amazon.awscdk.services.s3.EventType;
-import software.amazon.awscdk.services.s3.IBucket;
-import software.amazon.awscdk.services.s3.LifecycleRule;
-import software.amazon.awscdk.services.s3.StorageClass;
-import software.amazon.awscdk.services.s3.Transition;
-import software.amazon.awscdk.services.s3.notifications.SnsDestination;
-import software.amazon.awscdk.services.sns.ITopicSubscription;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
-import software.constructs.Construct;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static com.likedancesport.util.DevOpsConstants.*;
 
 @Component
 public class ComputeServerlessStack extends Stack {
     @Autowired
-    public ComputeServerlessStack(final Construct scope, final String id, @NotNull final StackProps props) {
+    public ComputeServerlessStack(final App scope, final String id, @NotNull final StackProps props) {
         super(scope, id, props);
     }
 }
