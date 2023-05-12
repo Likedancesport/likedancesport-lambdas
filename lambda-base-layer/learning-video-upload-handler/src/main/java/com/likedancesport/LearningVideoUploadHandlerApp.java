ea577b3d5b11b9f16aa75c068bba661263add858
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-05-12 17:01:19 +0300

    created job complete handler lambda

--- lambda-base-layer/learning-video-upload-handler/src/main/java/com/likedancesport/LearningVideoUploadHandlerApp.java
+++ lambda-base-layer/learning-video-upload-handler/src/main/java/com/likedancesport/LearningVideoUploadHandlerApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningVideoUploadHandlerApp {
-    //TODO: Rebuild config for db
     public static void main(String[] args) {
         SpringApplication.run(LearningVideoUploadHandlerApp.class, args);
     }
