fbd8f1e358fe698c8fc3a66d01d66b30ccf4f499
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-05-16 13:23:12 +0300

    refactored headers generating

--- lambda-base-layer/learning-video-transcoding-job-complete-handler/src/main/java/com/likedancesport/LearningVideoTranscodingJobCompleteHandlerApplication.java
+++ lambda-base-layer/learning-video-transcoding-job-complete-handler/src/main/java/com/likedancesport/LearningVideoTranscodingJobCompleteHandlerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningVideoTranscodingJobCompleteHandlerApplication {
     public static void main(String[] args) {
+        System.out.println("----- STARTUP");
         SpringApplication.run(LearningVideoTranscodingJobCompleteHandlerApplication.class, args);
     }
 }
